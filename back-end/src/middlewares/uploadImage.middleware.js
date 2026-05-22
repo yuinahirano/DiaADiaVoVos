deb9df5ac3fb496847a3c4316403af2b5597fd98
DANIELLY RODRIGUES FIGUEIREDO <danielly.figueiredo3@senaisp.edu.br>
2026-05-22 13:15:09 -0300

    :bricks: criada estrutura de pastas do projeto back-end!

--- back-end/src/middlewares/uploadImage.middleware.js
+++
@@ -1,9 +0,0 @@
-import createMulter from "../configs/upload.multer.js";
-
- const uploadImage = createMulter({
-    pasta: 'imagens',
-    tiposPermitidos: ['image/png', 'image/jpeg'],
-    tamanhoArquivo: 10 * 1024 * 1024
-}).single('imagem');
-
-export default uploadImage;
