cee1ca3e35ff8569175cdf50149596bc7e6dc9dd
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-08-14 09:13:27 -0300

    ✨ feat: adiciona o routes responsavel pelo relacionamento de idoso/cuidador

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import usuarioRoutes from "./usuario.routes";
 import cuidadorRoutes from "./cuiador.routes";
 import idosoRoutes from "./idoso.routes";
+import idosoCuidadorRoutes from "./idosoCuidador.routes";
 
 const router = Router();
 
 router.use('/', usuarioRoutes);
 router.use('/', idosoRoutes);
 router.use('/', cuidadorRoutes);
+router.use('/', idosoCuidadorRoutes);
 
 export default router;
