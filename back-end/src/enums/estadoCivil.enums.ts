2de0b6493fdca56dc14a5c6ad9563292e934a7e6
spskills <competidor10@competidor.com>
2026-08-14 10:24:31 -0300

    :books: docs: Ajustado os arquivo de documentação do backend

--- back-end/src/enums/estadoCivil.enums.ts
+++
@@ -1,7 +0,0 @@
-export enum EstadoCivil {
-  Solteiro = "solteiro",
-  Casado = "casado",
-  Divorciado = "divorciado",
-  Viuvo = "viúvo",
-  Separado = "separado"
-}
--- back-end/src/middlewares/uploadImage.middlewares.ts
+++
@@ -1,9 +0,0 @@
-import createMulter from "../config/multer.config";
-
-const uploadImage = createMulter({
-  folder: "Images",
-  allowedTypes: ["image/jpeg", "image/png"],
-  fileSize: 10 * 1024 * 1024,
-}).single("image");
-
-export default uploadImage;
