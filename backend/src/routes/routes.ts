742e44808367adfd8cd16bc194ccc99ccf073367
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-08-14 10:45:43 -0300

    ✨ feat: adiciona o routes responsavel pelo cadastgro de doenças

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,6 +4,7 @@ import cuidadorRoutes from "./cuiador.routes";
 import idosoRoutes from "./idoso.routes";
 import idosoCuidadorRoutes from "./idosoCuidador.routes";
 import consultaRoutes from "./consulta.routes";
+import DoencaRoutes from "./doenca.routes";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/', idosoRoutes);
 router.use('/', cuidadorRoutes);
 router.use('/', idosoCuidadorRoutes);
 router.use('/', consultaRoutes);
+router.use('/', DoencaRoutes);
 
 export default router;
