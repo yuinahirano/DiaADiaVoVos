4374a0c4f50006ad33a0f5603483ad847ca9b207
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-08-21 12:10:54 -0300

    :sparkles: feat: Cria o controller de admin

---
+++ backend/src/controller/admin.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import { AdminService } from "../services/admin.services";
+
+export class AdminController {
+  constructor(private _service = new AdminService()) {}
+  selecionar = async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id ? String(req.params.id) : null;
+      let result = null;
+      if (id) {
+        result = await this._service.selecionarPorId(id);
+        return res.status(200).json({ result });
+      }
+      result = await this._service.selecionarTodos();
+      return res.status(200).json({ result });
+    } catch (error) {
+      console.error(error);
+      const message =
+        error instanceof Error ? error.message : "Erro desconhecido";
+      return res.status(500).json({
+        message: "Ocorreu um erro no servidor",
+        errorMessage: message,
+      });
+    }
+  };
+
+  criar = async (req: Request, res: Response) => {
+    try {
+      const { nome, email, senha } = req.body;
+      const novo = await this._service.criar(
+        nome,
+        email,
+        senha
+      );
+      res.status(201).json({ novo });
+    } catch (error: unknown) {
+      console.error(error);
+      const message =
+        error instanceof Error ? error.message : "Erro desconhecido";
+      return res.status(500).json({
+        message: "Ocorreu um erro no servidor",
+        errorMessage: message,
+      });
+    }
+  };
+
+editar = async (req: Request, res: Response) => {
+  try {
+    const id = String(req.params.id);
+    const { nome, email, senha } = req.body;
+    const editado = await this._service.editar(
+      id,
+      nome,
+      email,
+      senha
+    );
+    res.status(200).json({ editado });
+  } catch (error: unknown) {
+    console.error(error);
+    const message =
+      error instanceof Error ? error.message : "Erro desconhecido";
+    return res.status(500).json({
+      message: "Ocorreu um erro no servidor",
+      errorMessage: message,
+    });
+  }
+  };
+
+  deletar = async (req: Request, res: Response) => {
+    try {
+      const id = String(req.params.id);
+      const deletado = await this._service.deletar(id);
+      res.status(200).json({ deletado });
+    } catch (error: unknown) {
+      console.error(error);
+      const message =
+        error instanceof Error ? error.message : "Erro desconhecido";
+      return res.status(500).json({
+        message: "Ocorreu um erro no servidor",
+        errorMessage: message,
+      });
+    }
+  };
+    login = async (req: Request, res: Response) => {
+    try {
+      const { email, senha } = req.body;
+      const login = await this._service.login(email, senha);
+      res.status(200).json({ login });
+    } catch (error: unknown) {
+      console.error(error);
+      const message = error instanceof Error ? error.message : "Erro desconhecido";
+      return res.status(500).json({ message: "Ocorreu um erro no servidor", errorMessage: message });
+    }
+  };
+}
