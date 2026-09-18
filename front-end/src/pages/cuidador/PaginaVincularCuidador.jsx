d6040cc41a59f3555d9cc075326bf13323cc3f5f
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-09-18 14:56:33 -0300

    Commit para merge

--- front-end/src/pages/cuidador/PaginaVincularCuidador.jsx
+++ front-end/src/pages/cuidador/PaginaVincularCuidador.jsx
@@ -1,9 +1,6 @@
 //import React, { useState } from 'react';
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-//import { vincularIdosoCuidador } from '../../service/userApi';
-import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { vincularIdosoCuidador } from "../../service/userApi";
 import { criarSolicitacaoCuidador } from "../../service/solicitacaoApi";
 import { AuthContext } from "../../contexts/AuthContext";
